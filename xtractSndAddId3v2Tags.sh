8eea49fd7b9cd5c725db9518b9af9e7a56f3e9ee
paolot <paolot@debian>
2015-10-26 09:04:03 +0100

    updated for ASPMA course

--- xtractSndAddId3v2Tags.sh
+++ xtractSndAddId3v2Tags.sh
@@ -2,7 +2,7 @@
 
 #chmod u+x xtractSndAddId3v2Tags.sh
 
-cd /home/paolot/gitstuff/coursera/introfinance-008/
+cd /mnt/disk2/courseraDownloads/coursera/audio-002/
 
 counter=0
 
@@ -11,15 +11,17 @@ do
     chapter=${directory/%/}
     echo $chapter
     for files in $directory/*.mp4
+#    for files in $directory/*.mp3
     do
 	let counter=counter+1
 	fileName=${files#0*//}
 	echo $fileName
 	file1="${fileName/%.mp4}.wav";
+#	file1="${fileName/%.mp3}.wav";
 	echo "extracting sound file" $fileName;
- 	mplayer -ao pcm:fast:file=$file1 -vo null -vc null -ni $files;
-	echo "processing extracted sound stream"
-	# increase volume
+	mplayer -ao pcm:fast:file=$file1 -vo null -vc null -ni $files;
+
+	echo "increase volume"
   	sox $file1 out.wav compand 0.3,1 6:-70,-60,-20 -5 -90 0.2
 	# optimize audio for head phone play
 	#	sox out.wav $file1 earwax
@@ -33,7 +35,9 @@ do
 	title=${title/[0-9][_]};
 	#file2="$title.ogg";
 	file2="$title.mp3"; # id3v2 tags seem not to work with ogg files 
- 	sox out.wav $file2 rate 8000 tempo -s 1.65 # process speech at 1.65 = 65% faster
+	echo "downsample and increase speed"
+	sox out.wav $file2 rate 8000 tempo -s 1.65 # process speech at 1.65 = 65% faster
+# 	sox out.wav $file2 rate 8000 # this guys speak quickly enough
 	
 	rm -f out.wav;
 	rm -f $file1;
@@ -41,14 +45,21 @@ do
 	echo "assign id3v2 tag"
  	id3v2 -D $file2;
 #  	id3v2 -2 $file2; # write only id3v2 tag
- 	id3v2 --TIT1 "Intro to finance" $file2;
- 	id3v2 --TIT2 "$title/Chapter $weekNum" $file2;
- 	id3v2 -a "Gautam Kaul" $file2;
- 	id3v2 -y "2008" $file2;
+	id3v2 --TIT1 "ASPMA course" $file2;
+	id3v2 --TIT2 "Chapter $weekNum" $file2;
+	id3v2 --TIT3 "$title" $file2;
+	id3v2 -a "Xavier Serra" $file2;
+	id3v2 -y "2015" $file2;
+#  	id3v2 --TIT1 "Economics of Money and Banking" $file2;
+#  	id3v2 --TIT2 "$title/Chapter $weekNum" $file2;
+#  	id3v2 -a "Perry G. Mehrling" $file2;
+#  	id3v2 -y "2015" $file2;
 # 
 #	id3v2 -T $str $file2;
-	id3v2 -T $counter $file2;
- 	id3v2 -A "week $weekNum" $file2;
+	id3v2 --TRCK $counter $file2;
+# 	id3v2 -A "week $weekNum" $file2;
+# 	id3v2 -A "Chapter $weekNum" $file2;
+	id3v2 --TALB "Audio Signal Processing for Music Applications" $file2;
 # 	echo $counter
     done
     
